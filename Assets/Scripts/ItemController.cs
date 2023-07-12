00a6019981c53e50a7d01ffb2f2f477932355c5d
DESKTOP-RVB4T1T\User <baekhua@gmail.com>
2023-07-12 23:13:33 +0900

    ,

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -1,30 +1,30 @@
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
     [SerializeField] GameObject _branch;
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Item"))
-    //    {
-    //        if (Input.GetKeyDown(KeyCode.F))
-    //        {
-    //            // 인벤토리에 들어감
-    //            _branch.SetActive(false);
-    //        }
-    //}
-    //}
-    //private void Update()
-    //{
-    //    if(Input.GetMouseButtonDown(0))
-    //    {
-    //        if(_branch != null)
-    //        {
-    //            _branch.SetActive(false);
-    //        }
-    //    }
-    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        float distance = Vector3.Distance(transform.position, other.transform.position);
+        if (other.CompareTag("Item"))
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                // 인벤토리에 들어감
+                _branch.SetActive(false);
+            }
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_branch != null)
+            {
+                _branch.SetActive(false);
+            }
+        }
+    }
 }
 class ItemObj
 {
